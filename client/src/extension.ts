6d2e50dc141febc1d5e1329528034a7e6b45dbd5
Oscar Ablinger <dev.ablinger@gmail.com>
2020-04-17 16:34:24 +0200

    feat(code): configure code launch

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -16,33 +16,28 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	// The server is implemented in dotnet
+	const serverExe = 'dotnet'
+
+	const serverPath = 'C:/Users/Oscar/Projects/master-thesis/autosupport-lsp-server/autosupport-lsp-server/bin/Release/netcoreapp3.1/autosupport-lsp-server.dll'
+	const descriptionFilePath = 'test1'
 
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+		run: { command: serverExe, args: [ 
+			serverPath, descriptionFilePath, 'test2'
+		] },
+		debug: { command: serverExe, args: [ 
+			serverPath, descriptionFilePath, 'test2'
+ 		] }
+	}
+
 
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
 		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
 	};
 
 	// Create the language client and start the client.
@@ -52,6 +47,8 @@ export function activate(context: ExtensionContext) {
 		serverOptions,
 		clientOptions
 	);
+
+	console.log('Client configured… starting server')
 
 	// Start the client. This will also launch the server
 	client.start();
